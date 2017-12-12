db11888b1b1eb92fc3322b3f24219a871e370819
gdznela <gdznela@example.com>
2017-12-13 00:36:27 +0400

    initial code and escription

---
+++ L605/script.js
@@ -0,0 +1,16 @@
+
+
+var students = [
+    { FirstName: 'დავით', LastName: 'წულუკიძე', PersonalNumber: '123'},
+    { FirstName: 'მიხილ', LastName: 'კაპანაძე', PersonalNumber: '124'},
+    { FirstName: 'სოფო', LastName: 'დოღონაძე', PersonalNumber: '125'}
+];
+
+
+
+function displayStudentsTable(studentList){
+    //write code
+}
+
+
+displayStudentsTable(studens);
